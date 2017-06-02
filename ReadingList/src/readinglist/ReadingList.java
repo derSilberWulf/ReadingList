0691c18b6ec2faad353700a21306f3b9df57bf3e
Vince <yahna.3@wright.edu>
2017-06-02 17:33:08 -0400

    added source code.

---
+++ ReadingList/src/readinglist/ReadingList.java
@@ -0,0 +1,18 @@
+package readinglist;
+
+/**
+ * Vincent Yahna
+ * June 2, 2017
+ * This project allows a user to keep an offline reading list for books read, 
+ * movies watched, etc. It uses SQLite for the database to keep things simple.
+ */
+public class ReadingList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
