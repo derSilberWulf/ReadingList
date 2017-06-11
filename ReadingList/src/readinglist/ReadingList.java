cfbe620e6e6f8961169a45105070dcf60a219034
unknown <yahna.3@wright.edu>
2017-06-11 17:09:53 -0400

    Finished creating tables and added methods in java project

--- ReadingList/src/readinglist/ReadingList.java
+++ ReadingList/src/readinglist/ReadingList.java
@@ -1,5 +1,14 @@
 package readinglist;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Vincent Yahna
  * June 2, 2017
@@ -11,8 +20,52 @@ public class ReadingList {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException, FileNotFoundException, IOException {
+        //TO DO: check if tables exist
+        Connection c = null;
+        try {
+          Class.forName("org.sqlite.JDBC");
+          c = DriverManager.getConnection("jdbc:sqlite:test.db");
+          createDatabase(c);
+
+          c.close();
+        } catch ( SQLException e ) {
+          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+          System.exit(0);
+        }
+        
+        
+    }
+    
+    /**
+     * Run all the create statements to create the database.
+     * @param c the connection to the database
+     */
+    public static void createDatabase(Connection c) throws SQLException, FileNotFoundException, IOException{
+          Statement stmt = c.createStatement();
+          String sql = getFileContents("..//create_tables.sql");
+          stmt.executeUpdate(sql);
+      
+          stmt.close();
+    }
+    
+     /**
+     * Gets the contents of a file and puts it into a string
+     * The file should be a small file so that it is okay to have it in memory
+     * @param path the path to the file
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @return a string of the contents of the file
+     */
+    public static String getFileContents(String path) throws FileNotFoundException, IOException{
+        File file = new File(path);
+        FileInputStream fis = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        fis.read(data);
+        fis.close();
+
+        String str = new String(data, "UTF-8");
+        return str;
     }
 
 }
--- create_tables.sql
+++ create_tables.sql
@@ -2,18 +2,94 @@
 --@author: Vincent Yahna
 
 CREATE TABLE media_types(
-  id int primary key identity(1,1),
-  name varchar(255),
-  description varchar(255)
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR2(255),
+  description VARCHAR2(255)--notes and summaries should probably be longer than this
 );
 
+CREATE TABLE tags(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR2(255),
+  description VARCHAR2(255)
+);
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username VARCHAR2(20)
+  --password hash might be added in future, but everything is local and unencrypted, so it doesn't matter too much.
+);
+
+CREATE TABLE series(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR2(255),
+  description VARCHAR2(255)
+);
+
+CREATE TABLE franchises(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR2(255),
+  description VARCHAR2(255)
+);
+
+CREATE TABLE authors(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  last_name VARCHAR2(255),
+  first_name VARCHAR2(255),
+  notes VARCHAR2(255)
+);
+
+CREATE TABLE publishers(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR2(255)
+);
+
+--Tables that reference other tables
+
 CREATE TABLE works (
-  id int primary key identity(1,1),
-  title varchar2(255) not null,
-  subtitle varchar2(255),
-  volume int,
-  summary varchar2(255),
-  notes varchar2(255),
-  own_copy bit,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR2(255) NOT NULL,
+  subtitle VARCHAR2(255),
+  volume INTEGER,
+  summary VARCHAR2(255),
+  notes VARCHAR2(255),
+  own_copy BIT,
   media_type_id REFERENCES media_types(id)
 );
+
+CREATE TABLE reading_sessions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  work_id REFERENCES works(id),
+  user_id REFERENCES users(id),
+  notes VARCHAR2(255), 
+  date_started DATETIME,
+  date_finished DATETIME,
+  rating INTEGER
+);
+
+--The linking tables
+
+CREATE TABLE lk_tags_works(
+  tag_id REFERENCES tags(id),
+  work_id REFERENCES works(id)
+);
+
+CREATE TABLE lk_series_works(
+  series_id REFERENCES series(id),
+  work_id REFERENCES works(id)
+);
+
+CREATE TABLE lk_series_franchises(
+  series_id REFERENCES series(id),
+  franchise_id REFERENCES franchises(id)
+);
+--can there really be more than one publisher?
+CREATE TABLE lk_publishers_works(
+  publisher_id REFERENCES publishers(id),
+  work_id REFERENCES works(id)
+);
+
+CREATE TABLE lk_authors_works(
+  author_id REFERENCES authors(id),
+  work_id REFERENCES works(id)
+);
+
