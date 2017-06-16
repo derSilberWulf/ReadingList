ff725da36215c4ee5ae8f7e17a6958a47870762d
unknown <yahna.3@wright.edu>
2017-06-15 21:48:45 -0400

    Updated sql for creating tables with NOT NULLs and increased VARCHAR size

--- create_tables.sql
+++ create_tables.sql
@@ -1,46 +1,48 @@
 --Creates tables for managing books, movies, etc. that a person has read
 --@author: Vincent Yahna
 
+--SQLite doesn't actually enforce limits on VARCHAR, due to limited data types being used,
+--but the tables will be written thusly in case there is a switch to an SQL server
 CREATE TABLE media_types(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR2(255),
-  description VARCHAR2(255)--notes and summaries should probably be longer than this
+  name VARCHAR2(255) NOT NULL,
+  description VARCHAR2(8000)
 );
 
 CREATE TABLE tags(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR2(255),
-  description VARCHAR2(255)
+  name VARCHAR2(255) NOT NULL,
+  description VARCHAR2(8000)
 );
 
 CREATE TABLE users(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username VARCHAR2(20)
+  username VARCHAR2(20) NOT NULL
   --password hash might be added in future, but everything is local and unencrypted, so it doesn't matter too much.
 );
 
 CREATE TABLE series(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title VARCHAR2(255),
-  description VARCHAR2(255)
+  title VARCHAR2(255) NOT NULL,
+  description VARCHAR2(8000)
 );
 
 CREATE TABLE franchises(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title VARCHAR2(255),
-  description VARCHAR2(255)
+  title VARCHAR2(255) NOT NULL,
+  description VARCHAR2(8000)
 );
 
 CREATE TABLE authors(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  last_name VARCHAR2(255),
+  last_name VARCHAR2(255) NOT NULL,
   first_name VARCHAR2(255),
-  notes VARCHAR2(255)
+  notes VARCHAR2(8000)
 );
 
 CREATE TABLE publishers(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR2(255)
+  name VARCHAR2(255) NOT NULL
 );
 
 --Tables that reference other tables
@@ -50,8 +52,8 @@ CREATE TABLE works (
   title VARCHAR2(255) NOT NULL,
   subtitle VARCHAR2(255),
   volume INTEGER,
-  summary VARCHAR2(255),
-  notes VARCHAR2(255),
+  summary VARCHAR2(8000),
+  notes VARCHAR2(8000),
   own_copy BIT,
   media_type_id REFERENCES media_types(id)
 );
@@ -60,7 +62,7 @@ CREATE TABLE reading_sessions (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   work_id REFERENCES works(id),
   user_id REFERENCES users(id),
-  notes VARCHAR2(255), 
+  notes VARCHAR2(8000), 
   date_started DATETIME,
   date_finished DATETIME,
   rating INTEGER
