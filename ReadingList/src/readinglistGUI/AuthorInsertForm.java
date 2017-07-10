187ccf4f0d1a32e1527ee9563f8bda68e06d3fd5
AlexY7 <yahna.2@wright.edu>
2017-07-09 21:03:49 -0400

    Made SQLEntryForm superclass to organize form GUIs components

--- ReadingList/src/readinglistGUI/AuthorInsertForm.java
+++ ReadingList/src/readinglistGUI/AuthorInsertForm.java
@@ -5,36 +5,28 @@
 package readinglistGUI;
 
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import javax.swing.*;
-import layout.SpringUtilities; 
 /**
  * JFrame that allows a user to enter data and submit it for a new author in 
  * the SQL table authors
  * @author Alexander Yahna
  */
-public class AuthorInsertForm extends JFrame {
+public class AuthorInsertForm extends SQLEntryForm {
     private static final int DEFAULT_WIDTH = 600;
     private static final int DEFAULT_HEIGHT = 300;
     
     private JTextField firstName;
     private JTextField lastName;
     private JTextArea notes;
-    private JButton submit; // user presses to submit entry
-    private JButton back; // allow user to go back to previous menu JFrame
+
     
     
     /**
      * default constructor : sets up form
      */
     public AuthorInsertForm(){
-        super("Add new Author to Database");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        this.setLayout(new BorderLayout());
+        super();
+        this.setTitle("Add new Author to Database");
         
         //initialize fields
         firstName = new JTextField(10);
@@ -42,41 +34,13 @@ public class AuthorInsertForm extends JFrame {
         lastName = new JTextField(10);
         notes = new JTextArea(2,2);
         
-        submit = new JButton("Submit");
-        back = new JButton("Back");
         
-        // create 3 Panels to organize content in JFrame with BorderLayout
-        JPanel centerPanel = new JPanel(new SpringLayout());
-        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        
-        // instructions go at top of GUI and submit button goes at bottom
-        northPanel.add(new JLabel("Fill out this form and click 'submit' to add an author to the database"));
-        southPanel.add(submit);
-        southPanel.add(back);
-        
+        super.setInstructions("Fill out this form and click 'submit' to add an author to the database");
         // text prompts for user - placed next to input forms
         String[] labels = {"First Name: ", "Last Name : ", "Notes (optional): "};
         JComponent[] inputs = {firstName, lastName, notes};
-        
-        // setup prompt/input area with SpringLayout
-        for(int i = 0; i < labels.length; ++i){
-            JLabel label = new JLabel(labels[i], JLabel.TRAILING);
-            centerPanel.add(label);
-            label.setLabelFor(inputs[i]);
-            centerPanel.add(inputs[i]);
-        }
-        
-        // Lay out the panel
-        SpringUtilities.makeCompactGrid(centerPanel,
-                                        labels.length , 2, //rows, columns
-                                        6,6,   //initX, initY
-                                        10,10);   //xPad, yPad
-        
-        // add panels
-        this.add(northPanel, BorderLayout.NORTH);
-        this.add(southPanel, BorderLayout.SOUTH);
-        this.add(new JScrollPane(centerPanel), BorderLayout.CENTER);
+        super.labelInputs(labels, inputs);
+
     }
     
     public static void main(String[] args){
---
+++ ReadingList/src/readinglistGUI/SQLEntryForm.java
@@ -0,0 +1,103 @@
+/*
+ * Template to make forms to for entering new SQL table columns
+ * Intended to be subclassed
+ */
+
+package readinglistGUI;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SpringLayout;
+import layout.SpringUtilities;
+
+/**
+ *
+ * @author Alexander Yahna
+ */
+public class SQLEntryForm extends JFrame {
+
+    private static final int DEFAULT_WIDTH = 600;
+    private static final int DEFAULT_HEIGHT = 300;
+    
+    private JPanel centerPanel;
+    private JLabel instructions;
+    private JButton submit; // user presses to submit entry
+    private JButton back; // allow user to go back to previous menu JFrame
+   
+    
+    SQLEntryForm(){
+    super();        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        this.setLayout(new BorderLayout());
+        
+        
+        submit = new JButton("Submit");
+        back = new JButton("Back");
+        
+        // create 3 Panels to organize content in JFrame with BorderLayout
+        centerPanel = new JPanel(new SpringLayout());
+        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        
+        // instructions go at top of GUI and submit button goes at bottom
+        instructions = new JLabel();
+        northPanel.add(instructions);
+        southPanel.add(submit);
+        southPanel.add(back);
+        
+  
+
+        // add panels
+        this.add(northPanel, BorderLayout.NORTH);
+        this.add(southPanel, BorderLayout.SOUTH);
+        this.add(new JScrollPane(centerPanel), BorderLayout.CENTER);
+    }
+    
+    /**
+     * Add inputs such as JTextFields and labels for the input to the CenterPanel
+     * of the GUI
+     * @param labels String array of labels
+     * @param inputs JComponent array of input fields
+     */
+    public void labelInputs(String[] labels, JComponent[] inputs){
+        // setup prompt/input area with SpringLayout
+        for(int i = 0; i < labels.length; ++i){
+            JLabel label = new JLabel(labels[i], JLabel.TRAILING);
+            centerPanel.add(label);
+            label.setLabelFor(inputs[i]);
+            centerPanel.add(inputs[i]);
+        }
+        
+        // Lay out the panel
+        SpringUtilities.makeCompactGrid(centerPanel,
+                                        labels.length , 2, //rows, columns
+                                        6,6,   //initX, initY
+                                        10,10);   //xPad, yPad
+    }
+    
+    /**
+     * Set the text instructions at the top of the Panel
+     * @param text 
+     */
+    public void setInstructions(String text){
+        instructions.setText(text);
+    }
+    
+    
+    /**
+     * Test class
+     * @param args 
+     */
+    public static void main(String[] args){
+        SQLEntryForm a = new SQLEntryForm();
+        a.setVisible(true);
+    }
+}
