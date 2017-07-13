40dbe6267a3f36f0c06c3083e5a715dae53b4fe5
AlexY7 <yahna.2@wright.edu>
2017-07-13 14:58:56 -0400

    Remade ActionSelectUI using NetBeans GUI builder environment.  ActionSelectGUI will be deleted Modified ReadingListModel to instantiate database connection and added getTrimmedTableList method

--- ReadingList/src/readinglist/ReadingListModel.java
+++ ReadingList/src/readinglist/ReadingListModel.java
@@ -5,10 +5,65 @@
 
 package readinglist;
 
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Alexander Yahna
  */
 public class ReadingListModel {
-    // to do
+    private ReadingListDatabase database; // accessor for class representing database
+    
+    public ReadingListModel(){
+        try {
+            database = new ReadingListDatabase();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ReadingListModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Getter for database
+     * @return ReadingListDatabase with connection to SQLite database
+     */
+    public ReadingListDatabase getDatabase(){
+        return database;
+    }
+    
+    /**
+     * Get a list of tables in the database without any link tables (denoted by
+     * starting with "lk" in this database). Also removes the SQLite sqlite_sequence
+     * table
+     * This method is a hack that might not be useful if not used with SQLite
+     * @return array of strings naming tables
+     */
+    public String[] getTrimmedTableList(){
+        String[] tables = {};
+        try {
+            tables = database.availableTables();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadingListModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ArrayList<String> tableList = new ArrayList<>();
+        Collections.addAll(tableList, tables);
+
+        tableList.remove("sqlite_sequence");
+
+        Iterator<String> itr = tableList.iterator();
+        String s;
+        while(itr.hasNext()){
+            s = itr.next();
+            if(s.startsWith("lk")){
+                itr.remove();
+            }
+        }
+        return tableList.toArray(new String[tableList.size()]);
+    }
+    
 }
